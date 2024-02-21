56ad6c3ac2817b56e6177e90dc310f3b1e31872a
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-02-21 16:27:45 +0300

    add the function for adding tasks in list

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
-console.log("Hello from ToDo App!");
+const form = document.querySelector("#form");
+const taskInput = document.querySelector("#taskInput");
+const tasksList = document.querySelector("#tasksList");
+const emptyList = document.querySelector("#emptyList");
+
+form.addEventListener("submit", addTask);
+
+function addTask(event) {
+  event.preventDefault();
+  const taskText = taskInput.value;
+  const taskHTML = `
+              <li class="list-group-item d-flex justify-content-between task-item">
+                  <span class="task-title">${taskText}</span>
+                  <div class="task-item__buttons">
+                      <button type="button" data-action="done" class="btn-action">
+                          <img src="./img/tick.svg" alt="Done" width="18" height="18">
+                      </button>
+                      <button type="button" data-action="delete" class="btn-action">
+                          <img src="./img/cross.svg" alt="Done" width="18" height="18">
+                      </button>
+                  </div>
+              </li>`;
+
+  //   console.log(taskList);
+
+  tasksList.insertAdjacentHTML(`beforeend`, taskHTML);
+
+  taskInput.value = "";
+  taskInput.focus();
+
+  if (tasksList.children.length > 1) {
+    emptyList.classList.add("none");
+  }
+}
