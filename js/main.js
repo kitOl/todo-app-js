32724c96e3e73633fdada79da55ca084e721304b
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-02-22 00:05:01 +0300

    add storage data in LocalStorage

--- js/main.js
+++ js/main.js
@@ -5,6 +5,31 @@ const emptyList = document.querySelector("#emptyList");
 
 let tasks = [];
 
+const localStorageData = localStorage.getItem("tasks");
+if (localStorageData) {
+  tasks = JSON.parse(localStorageData);
+}
+
+tasks.forEach(function (task) {
+  const classDone = task.done ? "task-title--done" : "";
+  const taskHTML = `
+                <li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+                    <span class="task-title ${classDone}">${task.text}</span>
+                    <div class="task-item__buttons">
+                        <button type="button" data-action="done" class="btn-action">
+                            <img src="./img/tick.svg" alt="Done" width="18" height="18">
+                        </button>
+                        <button type="button" data-action="delete" class="btn-action">
+                            <img src="./img/cross.svg" alt="Done" width="18" height="18">
+                        </button>
+                    </div>
+                </li>`;
+
+  tasksList.insertAdjacentHTML(`beforeend`, taskHTML);
+});
+
+checkEmptyList();
+
 form.addEventListener("submit", addTask);
 tasksList.addEventListener("click", deleteTask);
 tasksList.addEventListener("click", doneTask);
@@ -45,10 +70,12 @@ function addTask(event) {
   taskInput.value = "";
   taskInput.focus();
 
-  if (tasksList.children.length > 1) {
-    emptyList.classList.add("none");
-  }
+  //   if (tasksList.children.length > 1) {
+  //     emptyList.classList.add("none");
+  //   }
   //   saveHTMLtoLS();
+  saveToLocalStorage();
+  checkEmptyList();
 }
 
 function deleteTask(event) {
@@ -63,10 +90,13 @@ function deleteTask(event) {
     tasks = tasks.filter((task) => task.id !== Number(parentNode.id));
 
     parentNode.remove();
-    if (tasksList.children.length === 1) {
-      emptyList.classList.remove("none");
-    }
+
+    // if (tasksList.children.length === 1) {
+    //   emptyList.classList.remove("none");
+    // }
     // saveHTMLtoLS();
+    checkEmptyList();
+    saveToLocalStorage();
   }
 }
 
@@ -79,9 +109,29 @@ function doneTask(event) {
     taskTitle.classList.toggle("task-title--done");
 
     // saveHTMLtoLS();
+    saveToLocalStorage();
   }
 }
 
 function saveHTMLtoLS() {
   localStorage.setItem("tasksHTML", tasksList.innerHTML);
+}
+
+function checkEmptyList() {
+  if (!tasks.length) {
+    const emptyListElement = `
+        <li id="emptyList" class="list-group-item empty-list">
+            <img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3" />
+            <div class="empty-list__title">Список дел пуст</div>
+        </li>`;
+    tasksList.insertAdjacentHTML(`afterbegin`, emptyListElement);
+    console.log(tasks.length);
+  } else {
+    const emptyListEl = document.querySelector("#emptyList");
+    emptyListEl ? emptyListEl.remove() : null;
+  }
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
